b6a952e87ed7bfde1f2034ccd6dc06a8a985c167
Sherryn14 <147693979+Sherryn14@users.noreply.github.com>
2025-02-18 10:41:11 +0200

    Comment out the stop command

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ st.stop()
 
 #Convert the Snowpark Dataframe to Panda dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
